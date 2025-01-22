8494a59c073db08ed7419ae581a72fce5b4e7791
Angry Baker <checheninlive@mail.ru>
2025-01-22 16:48:38 +0300

    create field by js

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,22 @@ import GameField from "./game-field/GameField";
 
 // TODO: write code here
 document.addEventListener("DOMContentLoaded", () => {
+  const fieldElem = document.createElement("ul");
+  fieldElem.className = "game-field";
+  for (let i = 0; i < 16; i++) {
+    const cellElem = document.createElement("li");
+    cellElem.className = "game-cell";
+    fieldElem.appendChild(cellElem);
+  }
+
+  document.body.appendChild(fieldElem);
+
   const gameField = new GameField(document.querySelector(".game-field"));
   gameField.createGoblin();
   const jumpInterval = setInterval(() => {
     let newCell = gameField.changePosition();
     while (newCell === gameField.cellForGoblin) {
       newCell = gameField.changePosition();
-      console.log("ghbdtn")
     }
     newCell.appendChild(gameField.goblin);
   }, 1000);
